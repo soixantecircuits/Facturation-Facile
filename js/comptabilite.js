a680d3c98a75ccfebf54a054ab4d856da8e4958a
Gabrielstuff <gabriel@soixantecircuits.fr>
2012-01-04 18:45:14 +0100

    allowing to remove documents

--- js/comptabilite.js
+++ js/comptabilite.js
@@ -1,30 +1,28 @@
-$(document).ready(function() {
+$(function(){
 
 	$('.menu_link').click(function() {
 		ajax_page('#content', '<p style="text-align: center">Chargement ...</p>', this.href);
 		return false;
 	});
-
-
-	// Remove document
-	$('.delete_document').click(function() {
-		var datas = "operation=delete_document&datas=" + $(this).attr("id");
-
-		$.ajax({
-			type: "GET",
-			url: "ct-operations.php",
-			data: datas,
-			dataType: "json",
-			success: function(msg) {
-				ajax_page('#content', '<p style="text-align: center">Chargement ...</p>', 'ct-documents.php?section=' + msg.redirect);
-				return false;
-			}
-		});
-	});
 });
 
 /* Fonction de chargement ajax simple */
-
 function ajax_page(ele, msg, url) {
-	$(ele).html(msg).load(url);
+	$(ele).html(msg).load(url, function() {
+		// Remove document
+		$('.delete_document').click(function() {
+			var datas = "operation=delete_document&datas=" + $(this).attr("id");
+
+			$.ajax({
+				type: "GET",
+				url: "ct-operations.php",
+				data: datas,
+				dataType: "json",
+				success: function(msg) {
+					ajax_page('#content', '<p style="text-align: center">Chargement ...</p>', 'ct-documents.php?section=' + msg.redirect);
+					return false;
+				}
+			});
+		});
+	});
 }
